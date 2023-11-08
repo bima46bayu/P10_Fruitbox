c282418237d4cf516b694a25e7b35993f8694cbb
BIMA <bima46bayu25@gmail..com>
2023-11-08 21:52:12 +0700

    buahahahahaha

---
+++ app/src/main/java/com/example/p10_fruitbox/Fruit.kt
@@ -0,0 +1,10 @@
+package com.example.p10_fruitbox
+
+data class Fruit(
+    val name: String = "",
+    val rasa: String = "",
+    val harga: Int = 0,
+    val gambar: String = "",
+    val detail: String = "",
+
+)
